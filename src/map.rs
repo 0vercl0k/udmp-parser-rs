76a6ec2093e8822135967d15639c508a680f5c59
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2026-05-12 17:21:48 -0700

    unsafe block in unix

--- src/map.rs
+++ src/map.rs
@@ -218,7 +218,7 @@ cfg_select! {
             const MAP_SHARED: i32 = 1;
             const MAP_FAILED: *const u8 = usize::MAX as _;
 
-            extern "system" {
+            unsafe extern "system" {
                 fn mmap(
                     addr: *const u8,
                     length: usize,
